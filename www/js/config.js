5d1b8b91630d4848e93540d787ecd3e60816092f
Daniel Moczak <47257951+Moczak@users.noreply.github.com>
2019-02-01 22:58:51 -0200

    app-1.1

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={					
+	'ApiUrl':"https://delivery.oquevamospedir.com.br/merchantapp/api",
+	'DialogDefaultTitle':"",
+	'APIHasKey':"1c901f631aef492adda5a4c8c05ac8e0",
+	'debug': true
+};
