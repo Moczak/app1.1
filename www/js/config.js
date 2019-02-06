cde6768961e37119ceed1443875bf2d3d9f38f39
Daniel Moczak <47257951+Moczak@users.noreply.github.com>
2019-02-06 18:14:01 -0200

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={					
 	'ApiUrl':"https://delivery.oquevamospedir.com.br/merchantapp/api",
-	'DialogDefaultTitle':"",
+	'DialogDefaultTitle':"O Que Vamos Pedir?",
 	'APIHasKey':"1c901f631aef492adda5a4c8c05ac8e0",
 	'debug': true
 };
